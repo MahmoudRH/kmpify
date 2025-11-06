6166b9efa76e3f0e3fc62a62cff66b297482aa7a
Mahmoud Habib <mhabeeb5@students.iugaza.edu.ps>
2025-11-07 01:25:20 +0200

    - updated `replacePreviewImports()` to replace the imports for `PreviewParameter` and `PreviewParameterProvider` from androidx to jetbrains

--- migration/src/main/kotlin/mahmoud/habib/kmpify/core/KmpImport.kt
+++ migration/src/main/kotlin/mahmoud/habib/kmpify/core/KmpImport.kt
@@ -1,7 +1,9 @@
 package mahmoud.habib.kmpify.core
 
-enum class KmpImport(val import:String) {
+enum class KmpImport(val import: String) {
     JETBRAINS_PREVIEW("org.jetbrains.compose.ui.tooling.preview.Preview"),
+    JETBRAINS_PREVIEW_PARAMETER("org.jetbrains.compose.ui.tooling.preview.PreviewParameter"),
+    JETBRAINS_PREVIEW_PARAMETER_PROVIDER("org.jetbrains.compose.ui.tooling.preview.PreviewParameterProvider"),
     JETBRAINS_FONT("org.jetbrains.compose.resources.Font"),
     JETBRAINS_PAINTER_RESOURCE("org.jetbrains.compose.resources.painterResource"),
     JETBRAINS_STRING_RESOURCE("org.jetbrains.compose.resources.stringResource"),
